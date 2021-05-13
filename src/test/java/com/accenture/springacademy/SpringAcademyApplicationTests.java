727c819f49421a57be55d5c7b74f1ec9d3dc9fa2
Montero Santiago <j.montero.santiago@accenture.com>
2021-05-13 13:15:54 -0500

    initial commit

---
+++ src/test/java/com/accenture/springacademy/SpringAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.springacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
