5251c1fd0732297ad170f921bbed88041efa6f59
Montero Santiago <j.montero.santiago@accenture.com>
2021-05-14 10:21:33 -0500

    added spring JPA, H2 and Entity objects

--- src/main/java/com/accenture/springacademy/services/ProductService.java
+++ src/main/java/com/accenture/springacademy/services/ProductService.java
@@ -1,2 +1,49 @@
-package com.accenture.springacademy.services;public class ProductService {
+package com.accenture.springacademy.services;
+
+import com.accenture.springacademy.model.Product;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService {
+    //placeholder
+    private List<Product> productList = new ArrayList<>();
+
+    //From application.properties
+    @Value("${com.accenture.product.name}")
+    private String productName;
+    @Value("${com.accenture.product.cost}")
+    private Float productCost;
+
+    @PostConstruct
+    public void init(){
+        Product a = new Product("Chocomilk",20f);
+        Product b = new Product("PanBimbo", 15f);
+        Product c = new Product("Zucaritas", 10f);
+        Product d = new Product(productName,productCost);
+        productList.add(a);
+        productList.add(b);
+        productList.add(c);
+        productList.add(d);
+    }
+
+    public List<Product> getAllProducts(){
+        return productList;
+    }
+
+    public Product getProduct(String name){
+        List<Product> products = productList.stream().filter(item -> {
+            if(item.getName().equals(name)){
+                return true;
+            }
+            return false;
+        }).collect(Collectors.toList());
+
+        return products.get(0);
+    }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO PRODUCT (product_id, name, cost) VALUES (1, 'Eggs', 40);
+INSERT INTO PRODUCT (product_id, name, cost) VALUES (2, 'Milk', 20);
+INSERT INTO PRODUCT (product_id, name, cost) VALUES (3, 'Bread', 30);
+INSERT INTO PRODUCT (product_id, name, cost) VALUES (4, 'Cereal', 60);
+
+INSERT INTO STORE (store_id, name, city) VALUES (1, 'HEB', 'Monterrey');
+INSERT INTO STORE (store_id, name, city) VALUES (2, 'Walmart', 'Mexico');
+INSERT INTO STORE (store_id, name, city) VALUES (3, 'Sorina', 'Saltillo');
+INSERT INTO STORE (store_id, name, city) VALUES (4, 'Superama', 'Toluca');
+
+INSERT INTO CLIENT (client_id, first_name, last_name, email) values (1,'Jose','Hernandez','jhernandez@email.com');
+INSERT INTO CLIENT (client_id, first_name, last_name, email) values (2,'Maria','Lopez','mlopez@email.com');
+INSERT INTO CLIENT (client_id, first_name, last_name, email) values (3,'Juan','Garcia','jgarcia@email.com');
+INSERT INTO CLIENT (client_id, first_name, last_name, email) values (4,'Ana','Gutierrez','agutierrez@email.com');
+
+INSERT INTO PRODUCT_ORDER (client_id, product_id, date, quantity) values (1,1,sysdate,10);
+INSERT INTO PRODUCT_ORDER (client_id, product_id, date, quantity) values (1,2,sysdate,5);
+INSERT INTO PRODUCT_ORDER (client_id, product_id, date, quantity) values (1,3,sysdate,15);
